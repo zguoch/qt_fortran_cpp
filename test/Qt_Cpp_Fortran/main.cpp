e6f3052cab5a5ac3ab5d2d3ea46f3298ff76ffd7
Zhikui Guo <zhikuiguo@live.cn>
2024-10-30 13:34:44 +0800

    add test for Qt c++ fortran

---
+++ test/Qt_Cpp_Fortran/main.cpp
@@ -0,0 +1,14 @@
+#include <QCoreApplication>
+#include <QLibrary>
+#include <iostream>
+
+extern "C" {
+    void say_hello_();
+}
+
+int main(int argc, char *argv[]) {
+    QCoreApplication a(argc, argv);
+    say_hello_();
+    std::cout<<"hello qt fortran"<<std::endl;
+    return a.exec();
+}
---
+++ test/Qt_Cpp_Fortran/test.f90
@@ -0,0 +1,3 @@
+subroutine say_hello()
+    print *, "Hello from Fortran!"
+end subroutine say_hello 
