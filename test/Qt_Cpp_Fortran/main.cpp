bd6c326c6df55a925f1f1215d0bfcf70c8ed9d0d
Zhikui Guo <zhikuiguo@live.cn>
2024-11-06 18:40:38 +0800

    测试通过:qt_cpp_fortran

--- test/Qt_Cpp_Fortran/main.cpp
+++ test/Qt_Cpp_Fortran/main.cpp
@@ -2,13 +2,13 @@
 #include <QLibrary>
 #include <iostream>
 
-// extern "C" {
-//     void say_hello_();
-// }
+extern "C" {
+    void say_hello_();
+}
 
 int main(int argc, char *argv[]) {
     QCoreApplication a(argc, argv);
-    // say_hello_();
+    say_hello_();
     std::cout<<"hello qt fortran"<<std::endl;
     return a.exec();
 }
